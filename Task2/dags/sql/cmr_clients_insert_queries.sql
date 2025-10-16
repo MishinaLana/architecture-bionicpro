82cb32a1b096a783671c0d1f6974264011400d5c
DevGirl91 <nosova1997sveta@yandex.ru>
2025-10-16 21:08:53 +0300

    feature: Наработочки

---
+++ Task2/dags/sql/cmr_clients_insert_queries.sql
@@ -0,0 +1,2 @@
+INSERT INTO crm_client (email,device_id) VALUES ('user1@example.com', 'd4d95296-c169-446e-a2ab-516df0246d1e');
+INSERT INTO crm_client (email,device_id) VALUES ('prothetic3@example.com', 'b3bf6cdb-d959-4dac-bcc3-4713739364db');
